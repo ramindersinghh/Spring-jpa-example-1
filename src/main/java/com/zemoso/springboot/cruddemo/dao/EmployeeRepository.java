4ab4b76d867e405750fc9a50e80e4af06d141eb3
ramindersinghh <raminder.singh@zemosolabs.com>
2021-07-07 11:31:33 +0530

    Initial commit

---
+++ src/main/java/com/zemoso/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.zemoso.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zemoso.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+}
